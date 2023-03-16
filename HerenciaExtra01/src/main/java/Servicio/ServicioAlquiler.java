be485ac312e8d8c7835747b25a970c0bb303d264
Leonardo Rosero 'LERO <roseroleo@gmail.com>
2023-03-16 08:52:26 -0500

    En proceso extra 4 herencia

--- HerenciaExtra01/src/main/java/Servicio/ServicioAlquiler.java
+++ HerenciaExtra01/src/main/java/Servicio/ServicioAlquiler.java
@@ -21,10 +21,11 @@ import java.util.Iterator;
  */
 public class ServicioAlquiler {
 
-    public void CrearAlquiler() {
-
         //Crear un listado de alquileres
         ArrayList<Alquiler> alquiler = new ArrayList<>();
+    public void CrearAlquiler() {
+
+       
         
         //Lista de barcos
         BarcoYate y1 = new BarcoYate(300, 12, "La Pinta", "P0001", 30, 1990);
@@ -33,40 +34,35 @@ public class ServicioAlquiler {
         BarcoVelero v2 = new BarcoVelero(1, "La Santa Maria", "V004", 50, 1980);
         
         //Lista de alquileres
-        Alquiler a1 = new Alquiler("Juan", 1001, "01/01/2023", "30/01/2023", "A", y1);
-        Alquiler a2 = new Alquiler("Pedro", 1002, "01/01/2023", "25/01/2023", "B", v1);
-        Alquiler a3 = new Alquiler("Jose", 1003, "01/01/2023", "20/01/2023", "D", m1);
-        Alquiler a4 = new Alquiler("Rosa", 1004, "01/01/2023", "15/01/2023", "H", v2);
-        
-        //Adicionar alquileres
-        alquiler.add(a1);
-        alquiler.add(a2);
-        alquiler.add(a3);
-        alquiler.add(a4);
-
+        alquiler.add(new Alquiler("Juan", 1001, "01/01/2023", "30/01/2023", "A", y1));
+        alquiler.add(new Alquiler("Pedro", 1002, "01/01/2023", "25/01/2023", "B", v1));
+        alquiler.add(new Alquiler("Jose", 1003, "01/01/2023", "20/01/2023", "D", m1));
+        alquiler.add(new Alquiler("Rosa", 1004, "01/01/2023", "15/01/2023", "H", v2));
         //Mostrar el listado
         for (Alquiler aux : alquiler) {
             System.out.println(aux.toString());
-        }
     }
+        }
 
     //Calcular el tiempo de alquiler
     public long tiempoAlquiler() {
-        ArrayList<Alquiler> alquiler = new ArrayList<>();
         Iterator<Alquiler> it = alquiler.iterator();
         
-//        Alquiler a = new Alquiler();
         
-        System.out.println("fecha i "+a.getFechaAlquiler());
-        
-        //Dar formato a la fecha y llamar las fechas ingresadas
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate fechaInicial = LocalDate.parse(a.fechaAlquiler.toString(), formatter);
-        LocalDate fechaFinal = LocalDate.parse(a.fechaDevolucion.toString(), formatter);
-        //Calcular el tiempo de alquiler
-        long diasAlquiler = ChronoUnit.DAYS.between(fechaInicial, fechaFinal);
-        System.out.println("dias alquiler ="+diasAlquiler);
-        return diasAlquiler;
+//        Iterator<Alquiler> it = alquiler.iterator();
+//        
+////        Alquiler a = new Alquiler();
+//        
+//        System.out.println("fecha i "+a.getFechaAlquiler());
+//        
+//        //Dar formato a la fecha y llamar las fechas ingresadas
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+//        LocalDate fechaInicial = LocalDate.parse(a.fechaAlquiler.toString(), formatter);
+//        LocalDate fechaFinal = LocalDate.parse(a.fechaDevolucion.toString(), formatter);
+//        //Calcular el tiempo de alquiler
+//        long diasAlquiler = ChronoUnit.DAYS.between(fechaInicial, fechaFinal);
+//        System.out.println("dias alquiler ="+diasAlquiler);
+        return 0;// diasAlquiler;
     }
 
     //Calcular el valor del alquiler
